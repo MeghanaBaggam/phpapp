698c7f21dba8c57710de1160bea8adfe9b9451bf
Meghana <meghanabaggam@gmail.com>
2025-10-08 18:56:08 +0530

    nihar added

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ echo "<h1>Hello Yaminichakka</h1>";
 echo "<h1>SrujithaBaggam</h1>";
 echo "<h1>Hello SidduBaggam</h1>";
 echo "<h1>Abhitha</h1>";
-echo "<h>Sai Maneesh Patnaikggg</h1>";
+echo "<h>Sai Maneesh Patnaik</h1>";
+echo "<h>Niharrr</h1>";
 ?>
 
