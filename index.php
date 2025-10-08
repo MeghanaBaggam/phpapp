50b947b75be1d40ae9c23df7af7d9d573cf2c101
Meghana <meghanabaggam@gmail.com>
2025-10-08 18:48:07 +0530

    g added to maneesh

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ echo "<h1>Hello Yaminichakka</h1>";
 echo "<h1>SrujithaBaggam</h1>";
 echo "<h1>Hello SidduBaggam</h1>";
 echo "<h1>Abhitha</h1>";
-echo "<h>Sai Maneesh Patnaikg</h1>";
+echo "<h>Sai Maneesh Patnaikggg</h1>";
 ?>
 
