eb8e9624192bd0286600d26a46cb1d89f9aff3bb
Meghana <meghanabaggam@gmail.com>
2025-10-09 11:05:04 +0530

    cherry pick committt

--- index.php
+++ index.php
@@ -9,5 +9,7 @@ echo "<h>Sai Maneesh Patnaik</h1>";
 echo "<h>Niharrr</h1>";
 echo "<h1>My app is now bug free</h1>";
 echo "<h1>Helloo palss</h1>";
+echo "<h1>Cherry-pick1</h1>";
+echo "<h1>Cherry-pick2</h1>";
 ?>
 
